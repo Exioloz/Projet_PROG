7a9550d60d60ebd0ede3b27451f0aa42ece6ae36
Kimberly Beauvais <kimmyga01@gmail.com>
2022-01-05 17:58:46 +0100

    1.3-1.4 Edit

--- src/read_section2.c
+++ src/read_section2.c
@@ -1,18 +1,7 @@
-#include <stdio.h>
-#include <memory.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <memory.h>
-#include <string.h>
-#include "elf_header.h"
-#include "readelf.h"
-#include <elf.h>
-#include "elf_shdrs.h"
-#include <ctype.h>
+#include "read_section.h"
 
-void read_section(Filedata * filedata, char section_name);
-
-void read_section(Filedata * filedata, char section_name) {
+// add Section '.data' has no data to dump. if size == 0
+int read_section(Filedata * filedata, char * section_name) {
 
     rewind(filedata->file);
 
@@ -20,23 +9,31 @@ void read_section(Filedata * filedata, char section_name) {
 
     char shstrtab[filedata->section_headers[filedata->file_header.e_shstrndx].sh_size];
     char *temp = shstrtab;
-    a = fread(shstrtab, filedata->section_headers[filedata->file_header.e_shstrndx].sh_size, 1, filedata->file);
+    size_t a = fread(shstrtab, filedata->section_headers[filedata->file_header.e_shstrndx].sh_size, 1, filedata->file);
     if (0 == a){
         printf("\nfail to read\n");
     }
 
     // 遍历
     for (int i = 0; i < filedata->file_header.e_shnum; i++){
-        temp = shstrtab;
-        temp = temp + filedata->section_headers[i].sh_name;
+        temp = shstrtab + filedata->section_headers[i].sh_name;
         if (strcmp(temp, section_name) != 0) continue;//该section?~P~M?
         printf("\n");
         printf("Vidange hexadecimale de la section <<  %s>> :\n", temp);
         printf("Offset: %x\n", filedata->section_headers[i].sh_offset);
         printf("La taille: %x\n", filedata->section_headers[i].sh_size);
         char  *sign_data=(char*)malloc(sizeof(char)*filedata->section_headers[i].sh_size);
+        if (sign_data == NULL){
+            fprintf(stderr, "Memory allocation for sign data failed\n");
+            return false;
+        }
+
         fseek(filedata->file, filedata->section_headers[i].sh_offset, SEEK_SET);
-        fread(sign_data, sizeof(char)*(filedata->section_headers[i].sh_size), 1, filedata->file);
+        
+        
+        if (fread(sign_data, sizeof(char)*(filedata->section_headers[i].sh_size), 1, filedata->file)){
+            fprintf(stderr, "Unable to read file\n");
+        }
         char *p = sign_data;
         int j = 0;
         int c = 0;
@@ -53,7 +50,8 @@ void read_section(Filedata * filedata, char section_name) {
             if(c == 16){
                 printf(" ");
                 p = p-c;
-                for(int k=0; k<c; k++){if(*p!=0 && *p!=11 && *p!=18){
+                for(int k=0; k<c; k++){
+                    if(*p!=0 && *p!=11 && *p!=18){
                         printf("%c",*p);
                     }else{
                         printf(".");
@@ -88,8 +86,5 @@ void read_section(Filedata * filedata, char section_name) {
         }
         printf("\n");
     }
-
-
-
-    return 0;
+    return true;
 }
