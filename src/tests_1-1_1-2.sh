17cafa90f48de6c8a0b4c43b477e2a7a0e61f425
Hugo R <55627173+IHUN-ihateusernames@users.noreply.github.com>
2022-01-05 23:06:46 +0100

    Modified spacing for compatibilty with test file 2

--- src/tests_1-1_1-2.sh
+++ src/tests_1-1_1-2.sh
@@ -8,33 +8,11 @@ do
   readelf -hS $test > .original
   ./readelf $test > .custom
   sed -i '/Examples_loader/d' .custom
-  sed -i 's/Elf Header/ELF Header/' .custom
-  sed -i 's/Magic:/ Magic:/' .custom
-  sed -i 's/Class:/ Class:/' .custom
-  sed -i 's/Data:/ Data:/' .custom
-  sed -i 's/OS\/ABI:/ OS\/ABI:/' .custom
-  sed -i 's/Version:/ Version:/' .custom
-  sed -i 's/ABI Version / ABI Version:/' .custom
-  sed -i 's/Type:/ Type:/' .custom
-  sed -i 's/Machine:/ Machine:/' .custom
-  sed -i 's/Version / Version:/' .custom
-  sed -i 's/Entry point address / Entry point address:/' .custom
-  sed -i 's/Start of program headers / Start of program headers:/' .custom
-  sed -i 's/Start of section headers:/ Start of section headers:/' .custom
-  sed -i 's/Flags:/ Flags:/' .custom
-  sed -i 's/Size of this header:/ Size of this header:/' .custom
-  sed -i 's/Size of program headers:/ Size of program headers:/' .custom
-  sed -i 's/Number of program headers:/ Number of program headers:/' .custom
-  sed -i 's/Size of section headers:/ Size of section headers:/' .custom
-  sed -i 's/Number of section headers:/ Number of section headers:/' .custom
-  sed -i 's/Section header string table index:/ Section header string table index:/' .custom
-  sed -i 's/Key to /Key to/' .custom
   sed -i 's/  W (write)/W (write)/' .custom
   sed -i 's/  L (link order)/L (link order)/' .custom
   sed -i 's/  C (compressed), x (unknown), o (OS specific), E (exclude),  /C (compressed), x (unknown), o (OS specific), E (exclude),/' .custom
-
   sed -i 's/   y (purecode)/ y (purecode)/' .custom
-
+  sed -i 's/   y (purecode)/ y (purecode)/' .custom
 
 if diff .original .custom > .temp_res
 then
