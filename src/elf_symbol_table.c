56d8cd56cd0fd74b759a392a2b65a97d41592bb9
Xuan Li <Xuan.Li@etu.univ-savoie.fr>
2022-01-07 15:01:38 +0100

    Comments added

--- src/elf_symbol_table.c
+++ src/elf_symbol_table.c
@@ -1,8 +1,20 @@
 #include "elf_symbol_table.h"
 
+/*
+  Function: BigLittleSwa
+
+  Swaps octets to change value from big endian to little endian and vice versa
+  returns the value changed
+*/
 #define BigLittleSwap32(A) ((((uint32_t)(A) & 0xff000000) >> 24) |  (((uint32_t)(A) & 0x00ff0000) >> 8 ) | (((uint32_t)(A) & 0x0000ff00) << 8 ) | (((uint32_t)(A) & 0x000000ff) << 24))
 #define BigLittleSwap16(A) ((((uint16_t)(A) & 0xff00) >> 8 ) |  (((uint16_t)(A) & 0x00ff) << 8 ))
 
+/*
+  Function: get_st_type
+
+  Obtains st_info in symbol table ((i)&0xf)
+  returns the type corresponding
+*/
 char * get_st_type(Elf32_Sym *symtable, int i) {
     switch (symtable[i].st_info & 0xf) {
         case 0:
@@ -25,7 +37,12 @@ char * get_st_type(Elf32_Sym *symtable, int i) {
     }
 
 }
+/*
+  Function: get_st_bind
 
+  Obtains st_info in symbol table ((i)>>4)
+  returns the type corresponding
+*/
 char * get_st_bind(Elf32_Sym *symtable, int i){
     switch(symtable[i].st_info >> 4){
         case 0: return "LOCAL";
@@ -39,7 +56,12 @@ char * get_st_bind(Elf32_Sym *symtable, int i){
     }
 }
 
+/*
+  Function: get_index_strtab
 
+  Takes filedata and a pointer pointing to the start of .shstrtab section
+  returns the index of string table where locates the name '.strtab' and other info about it
+*/
 int get_index_strtab(Filedata * filedata, char * temp, int index_strtab){
     char * a = temp;
     for (int i=0; i < filedata->file_header.e_shnum; i++){
@@ -50,6 +72,13 @@ int get_index_strtab(Filedata * filedata, char * temp, int index_strtab){
     }
     return index_strtab;
 }
+
+/*
+  Function: get_index_symtab
+
+  Takes filedata and a pointer pointing to the start of .shstrtab section
+  returns the index of string table where locates the name '.symtab' and other info about it
+*/
 int get_index_symtab(Filedata * filedata, char * temp, int index_symtab){
     char * a = temp;
     for (int i = 0; i < filedata->file_header.e_shnum; i++){
@@ -61,19 +90,40 @@ int get_index_symtab(Filedata * filedata, char * temp, int index_symtab){
     return index_symtab;
 }
 
+/*
+  Function: get_st_value
+
+  returns st value
+*/
 int get_st_value(Elf32_Sym *symtable, int i){
     return symtable[i].st_value;
 }
 
+/*
+  Function: get_st_name
+
+  Uses a pointer pointing to the start of .strtab and st name in symtable
+  returns a string which shows the real name
+*/
 char * get_st_name(Elf32_Sym *symtable, int i,char * strtemp){
     strtemp=strtemp+BigLittleSwap32(symtable[i].st_name);
     return strtemp;
 }
 
+/*
+  Function: get_st_value
+
+  returns st size
+*/
 int get_st_size(Elf32_Sym *symtable, int i){
     return symtable[i].st_size;
 }
+/*
+  Function: get_st_vis
 
+  Obtains st_other in symbol table
+  returns the visibility corresponding
+*/
 char * get_st_vis(unsigned int other){
     switch(other){
         case 0: return "DEFAULT\t";
@@ -81,6 +131,13 @@ char * get_st_vis(unsigned int other){
     }
 
 }
+
+/*
+  Function: get_rel_table
+
+    Gets the data relating to symbol table from the ELF file
+    and puts it into filedata.
+*/
 
 bool get_symbol_table(Filedata * filedata) {
 
@@ -126,6 +183,17 @@ bool get_symbol_table(Filedata * filedata) {
 
     return true;
 }
+
+/*================================================================
+    Process symbol table from Obtained Data
+  ================================================================*/
+
+/*
+Process Symbol Table
+Input : filedata
+Output : returns a bool to determine whether symbol table was processed properly
+Use the data in the structure to print as readelf
+*/
 
 bool process_symbol_table(Filedata * filedata){
     Elf32_Sym_Tab sym_tab = filedata->symbol_table;
