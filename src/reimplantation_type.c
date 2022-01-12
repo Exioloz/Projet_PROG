9bb3268b442fade4635b95f5912c0830ae96a221
Xuan Li <Xuan.Li@etu.univ-savoie.fr>
2022-01-13 00:51:14 +0100

    Etape 2.8 & 2.9 New version 3.0

--- src/reimplantation_type.c
+++ src/reimplantation_type.c
@@ -31,26 +31,20 @@ void calcul_val(Elf32_Ext_Rel *  ext_tab, Elf32_Addr * addr,Elf32_Off offset_sec
             case R_ARM_ABS16:
             case R_ARM_ABS8: //S
                 idx = ELF32_R_SYM(change_endian_32(ext_tab[index].rel_ents[i].r_info));
-                printf("nom:%s\n",get_section_name(filedata, filedata->section_headers[change_endian_16(symtable[idx].st_shndx)].sh_name));
-                printf("idx:%d\n",idx);
+
                 val_symbol=(uint32_t)(get_st_value(new_sym_tab, idx));
                 *addr_p = *addr;
                 *addr_p=offset_sec+change_endian_32(ext_tab[index].rel_ents[i].r_offset);
-                printf("Ofset:%d\n",change_endian_32(ext_tab[index].rel_ents[i].r_offset));
                 replace_data(filedata, val_symbol, addr_p, newfile);//replace the value starts at p by addr_symbol
                 free(addr_p);
                 break;
             case R_ARM_CALL:
             case R_ARM_JUMP24: //S - P
                 idx = ELF32_R_SYM(change_endian_32(ext_tab[index].rel_ents[i].r_info));
-                printf("nom:%s\n",get_section_name(filedata, filedata->section_headers[change_endian_16(symtable[idx].st_shndx)].sh_name));
-                printf("idx:%d\n",idx);
                 val_symbol=(uint32_t)change_endian_32(get_st_value(new_sym_tab, idx));
                 val_symbol=(val_symbol-(*addr+change_endian_32(ext_tab[index].rel_ents[i].r_offset)));
-                val_symbol=change_endian_32(((uint16_t)val_symbol & 0x03FFFFFE)>>2);
-
+                val_symbol=change_endian_32((val_symbol & 0x03FFFFFE)>>2);
                 *addr_p=offset_sec+change_endian_32(ext_tab[index].rel_ents[i].r_offset);
-                printf("Ofset:%d\n",change_endian_32(ext_tab[index].rel_ents[i].r_offset));
                 replace_data(filedata,val_symbol, addr_p, newfile);
                 free(addr_p);
                 break;
@@ -64,21 +58,55 @@ void calcul_val(Elf32_Ext_Rel *  ext_tab, Elf32_Addr * addr,Elf32_Off offset_sec
 
 
 
+
 void replace_data(Filedata * filedata, uint32_t val_symbol, Elf32_Addr * addr_p, Filedata * newfile){
-    int a = 0;
+        int a = 0;
+//        printf("\nVal:%x\n",val_symbol);
+//        printf("\nAddr_p:%x\n",*addr_p);
 
-    uint32_t val;
+        if (newfile->file == NULL){
+            fprintf(stderr,"Input file %s is not readble.\n", newfile->file_name);
+            free_filedata(newfile);
+        }
 
-    if (newfile->file == NULL){
-        fprintf(stderr,"Input file %s is not readble.\n", newfile->file_name);
-        free_filedata(newfile);
+    a = fseek(newfile->file,*addr_p,SEEK_SET);
+
+    uint8_t *data = malloc(sizeof(uint8_t)*4);
+    a = fread(data, sizeof(uint32_t),1,newfile->file);
+
+    uint8_t *data1 = malloc(sizeof(uint8_t)*4);
+    memmove(data1, &val_symbol, 4);
+//    printf("data: %x, %x, %x, %x\n", data[0], data[1], data[2],data[3]);
+//    printf("data1: %x, %x, %x, %x\n", data1[0], data1[1], data1[2],data1[3]);
+    if(data[1] == 0xff || data[1] == 0){
+        data[1]=data1[1];
+        printf("1\n");
+
     }
-    //  rewind(newfile->file);
+    else{
+        data[1]+=data1[1];
+    }
+
+    if(data[2] == 0xff || data[2] == 0){
+        data[2]=data1[2];
+        printf("2\n");
+    }
+    else{
+        data[2]+=data1[2];
+    }
+
+    if(data[3] == 0xff || data[3] == 0){
+        data[3]=data1[3];
+        printf("3\n");
+    }
+    else{
+        data[3]+=data1[3];
+    }
+//    printf("data1: %x, %x, %x, %x\n", data1[0], data1[1], data1[2],data1[3]);
+
     a = fseek(newfile->file,*addr_p,SEEK_SET);
-    fread(&val, sizeof(uint32_t),1,newfile->file);
-    val_symbol=val_symbol+val;
-    a = fseek(newfile->file,*addr_p,SEEK_SET);
-    a = fwrite( &val_symbol, sizeof(uint32_t) , 1, newfile->file);
+    a = fwrite( data, sizeof(uint32_t) , 1, newfile->file);
+
     if (1 != a) {
         printf("\nfail to write\n");
     }
