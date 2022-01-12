2787c5e601780bb7985af90c7acdc183a2eca17a
Xuan Li <Xuan.Li@etu.univ-savoie.fr>
2022-01-12 16:14:39 +0100

    Etape 2.8 & 2.9 New version 2

--- src/reimplantation_type.c
+++ src/reimplantation_type.c
@@ -13,13 +13,13 @@ void implantation(Elf32_Addr * text_addr, Elf32_Addr * data_addr, Filedata * fil
 
 }
 
-void calcul_val(Elf32_Ext_Rel *  ext_tab, Elf32_Addr * addr, Filedata * filedata, int index, Filedata * newfile){
+void calcul_val(Elf32_Ext_Rel *  ext_tab, Elf32_Addr * addr,Elf32_Off offset_sec, Filedata * filedata, int index, Filedata * newfile){
     Elf32_Sym* symtable = filedata->symbol_table.sym_entries;
     uint32_t val_symbol=0;
     int idx=0;
     Elf32_Sym *new_sym_tab = newfile->symbol_table.sym_entries;
-    Elf32_Off offset_sec= filedata->section_headers[2].sh_offset;
-//    printf("Offset:%d\n", offset_sec);
+
+    printf("Offset:%d\n", offset_sec);
     for (int i = 0; i < ext_tab[index].rel_ent_num; i++){
         Elf32_Addr * addr_p= malloc(sizeof(Elf32_Addr));
         switch (ELF32_R_TYPE(change_endian_32(ext_tab[index].rel_ents[i].r_info))){
@@ -29,9 +29,11 @@ void calcul_val(Elf32_Ext_Rel *  ext_tab, Elf32_Addr * addr, Filedata * filedata
                 idx = ELF32_R_SYM(change_endian_32(ext_tab[index].rel_ents[i].r_info));
                 printf("nom:%s\n",get_section_name(filedata, filedata->section_headers[change_endian_16(symtable[idx].st_shndx)].sh_name));
                 printf("idx:%d\n",idx);
-                val_symbol=(uint32_t)change_endian_32(get_st_value(new_sym_tab, idx));
-                //      *addr_p = *addr;
+                val_symbol=(uint32_t)(get_st_value(new_sym_tab, idx));
+                *addr_p = *addr;
                 *addr_p=offset_sec+change_endian_32(ext_tab[index].rel_ents[i].r_offset);
+                printf("Ofset:%d\n",change_endian_32(ext_tab[index].rel_ents[i].r_offset));
+                //   *addr_p=*addr_p+change_endian_32(ext_tab[index].rel_ents[i].r_offset);
                 replace_data(filedata, val_symbol, addr_p, newfile);//replace the value starts at p by addr_symbol
                 free(addr_p);
                 break;
@@ -40,11 +42,13 @@ void calcul_val(Elf32_Ext_Rel *  ext_tab, Elf32_Addr * addr, Filedata * filedata
                 idx = ELF32_R_SYM(change_endian_32(ext_tab[index].rel_ents[i].r_info));
                 printf("nom:%s\n",get_section_name(filedata, filedata->section_headers[change_endian_16(symtable[idx].st_shndx)].sh_name));
                 printf("idx:%d\n",idx);
-                val_symbol=change_endian_32(get_st_value(new_sym_tab, idx));
+                val_symbol=(uint32_t)change_endian_32(get_st_value(new_sym_tab, idx));
                 //      *addr_p = *addr;
                 val_symbol=val_symbol-(*addr+change_endian_32(ext_tab[index].rel_ents[i].r_offset));
-                val_symbol=val_symbol & 0x03FFFFFE;
+                val_symbol=change_endian_32(val_symbol & 0x03FFFFFE);
+
                 *addr_p=offset_sec+change_endian_32(ext_tab[index].rel_ents[i].r_offset);
+                printf("Ofset:%d\n",change_endian_32(ext_tab[index].rel_ents[i].r_offset));
                 replace_data(filedata,val_symbol, addr_p, newfile);
                 free(addr_p);
                 break;
@@ -65,11 +69,9 @@ void replace_data(Filedata * filedata, uint32_t val_symbol, Elf32_Addr * addr_p,
         fprintf(stderr,"Input file %s is not readble.\n", newfile->file_name);
         free_filedata(newfile);
     }
-    //  rewind(newfile->file);
     a = fseek(newfile->file,*addr_p,SEEK_SET);
-
     a = fwrite( &val_symbol, sizeof(uint32_t) , 1, newfile->file);
-    if (0 == a) {
+    if (1 != a) {
         printf("\nfail to write\n");
     }
 }
